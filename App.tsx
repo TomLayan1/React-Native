8f8923c49d597d4454d7f1b0d53855d9e9b3ce1e
TomLayan1 <afolayantomisin1919@gmail.com>
2025-08-18 20:28:50 +0100

    Updated the Dimension API to adjust layout based on device orientation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 // Lesson 1: Change the text in the Text element to 'Hello World' and background of the the View element
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Dimensions, Image, Modal, Pressable, ScrollView, StatusBar, StyleSheet, Text, View } from "react-native";
 const logoImg = require('./assets/adaptive-icon.png')
 
@@ -14,20 +14,39 @@ export default function App() {
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false)
   const [darkTheme, setDarkTheme] = useState<boolean>(false);
   const theme = darkTheme ? 'black' : 'white'
+  const [dimension, setDimension] = useState({
+    window: Dimensions.get('window')
+  })
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener('change', ({ window }) => {
+      setDimension({ window: window })
+    })
+
+    return () => subscription.remove();
+  }, [])
+
+  const { window } = dimension;
+
+  const windowWidth = window.width;
+  const windowHeight = window.height;
 
   return (
     <View style={{ flex: 1, paddingVertical: 30, backgroundColor: theme}}>
       <StatusBar />
       <ScrollView style={style.pgScroll}>
         <Pressable onPress={() => console.log('image pressed')}>
-          <Image style={style.image} source={{uri: 'https://picsum.photos/200' }} />
+          <Image style={[style.image, {
+            height: windowHeight > 700 ? '70%' : '35%',
+            width: windowWidth > 500 ? '60%' : '30%'
+          }]} source={{uri: 'https://picsum.photos/200' }} />
         </Pressable>
         <Pressable onPress={() => console.log('Text pressed')}>
           <Text style={style.OuterText}>
             <Text style={style.worldText}>Hello </Text>World
           </Text>
         </Pressable>
-        <Text style={style.loremText}>
+        <Text style={{ fontSize: windowWidth > 500 ? 30 : 20, lineHeight: windowHeight > 700 ? 40 : 28 }}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac tellus ut nulla egestas aliquet. Suspendisse pulvinar nulla id quam pulvinar congue. Praesent accumsan semper risus, in finibus neque efficitur quis. Aliquam pellentesque erat id lacinia aliquet. Fusce elementum non purus a vulputate. Cras vestibulum orci sit amet sem egestas, sed ornare lectus bibendum. Donec ut euismod lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras ut nisl mauris. Pellentesque varius urna sed odio mattis condimentum. Quisque id lacus a libero venenatis hendrerit. Interdum et malesuada fames ac ante ipsum primis in faucibus.
           Donec dolor purus, interdum a iaculis vel, tristique vel nulla. Vivamus et felis condimentum, euismod nunc finibus, sodales arcu. Pellentesque fringilla vestibulum quam non ornare. Fusce bibendum lorem in lacinia convallis. Proin vitae laoreet augue. Integer accumsan urna ac eros sagittis venenatis. Aenean ut urna massa. Donec venenatis sollicitudin consequat. Donec rutrum elit nec dapibus vulputate.
         </Text>
@@ -49,7 +68,7 @@ export default function App() {
         />
       </ScrollView>
       <Modal visible={isModalVisible} onRequestClose={() => setIsModalVisible(false)}>
-        <Text>This is a modal and it works just fine.</Text>
+        <Text style={{ fontSize: windowWidth > 500 ? 30 : 20 }}>This is a modal and it works just fine.</Text>
         <Button
           title='Hide Modal'
           onPress={() => setIsModalVisible(false)}
@@ -70,8 +89,8 @@ const style = StyleSheet.create({
   },
   image: {
     margin: 'auto',
-    height: windowHeight > 700 ? '70%' : '35%',
-    width: windowWidth > 500 ? '60%' : '30%',
+    // height: windowHeight > 700 ? '70%' : '35%',
+    // width: windowWidth > 500 ? '60%' : '30%',
     marginBottom: 7
   },
   OuterText: {
@@ -84,11 +103,11 @@ const style = StyleSheet.create({
   worldText: {
     color: '#bab9b9ff',
   },
-  loremText: {
-    color: '#bab9b9ff',
-    fontSize: windowWidth > 500 ? 30 : 20,
-    lineHeight: windowHeight > 700 ? 40 : 28,
-  },
+  // text: {
+  //   color: '#bab9b9ff',
+  //   fontSize: windowWidth > 500 ? 30 : 20,
+  //   lineHeight: windowHeight > 700 ? 40 : 28,
+  // },
   pressIn : {
     backgroundColor: 'yellow',
     padding: 7,
